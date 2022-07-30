6710590c1a8a87092802b78e4d7abb559ecc7cf5
Krishan <kslg257@gmail.com>
2022-07-30 07:55:49 +0000

    CRUD Examples

--- sql-crud.py
+++ sql-crud.py
@@ -88,17 +88,69 @@ krishan_gharu = Programmer(
     famous_for="Learning how to code"
 )
 
-# add each instance of our programmers to our session
+# CREATE - add each instance of our programmers to our session
 # session.add(ada_lovelace)
 # session.add(alan_turing)
 # session.add(grace_hopper)
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(krishan_gharu)
+# session.add(krishan_gharu)
+
+# UPDATE - updating a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
 
 # commit our session to the database
 session.commit()
+
+# UPDATE - updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+# UPDATE - updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.nationality == "British":
+#         person.nationality = "UK Brit"
+#     elif person.nationality == "American":
+#         person.nationality = "US American"
+#     elif person.nationality == "Indian":
+#         person.nationality = "UK Brit"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+# DELETE - deleting a single record
+fname = input("Enter a first name: ")
+lname = input("Enter a last name: ")
+programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+if programmer is not None:
+    print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmer has been deleted")
+    else:
+        print("Programmer not deleted")
+else:
+    print("No records found")
+
+
+# **** WARNING *** delete multiple/all records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
 
 
 # query the database to find all Programmers
